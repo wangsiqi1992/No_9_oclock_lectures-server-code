86a4102d8c092bf387e44af702d474e7ffb7f093
Bill Wang <wangsiqi1992@hotmail.com>
2012-04-07 14:49:22 +0100

    tagManager
    manage anything involved tags,
    tags including author? tags? date…etc…

--- myFirstRESTserver/TagsManager.php
+++ myFirstRESTserver/TagsManager.php
@@ -11,6 +11,41 @@ class TagsManager {
     function __construct() {
         
     }
+    
+    /**
+     *
+     * @abstract        used by news controller to return data~!
+     * @param type $tags 
+     * @return          array of News
+     */
+    public function searchNewsWithTags($tags)
+    {
+        
+    }
+    
+    
+    /**
+     *
+     * @abstract        check tags exist, put different tags into DB, tags including: tags, author, date......etc.......
+     * @param type $nid
+     * @param type $tags 
+     * @return          scuccess or not~!
+     */
+    public function tagNews($nid, $tags)
+    {
+        
+    }
+    
+    
+    /**
+     *
+     * @abstract        summarise the structure of our tag system
+     * @todo            all the function needed for mobile UI structure....!!! Important, but not now..... 
+     */
+    public function getTagStructure()
+    {
+        
+    }
 
 }
 ?>
